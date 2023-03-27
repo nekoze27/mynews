010926360aaed61fe6fad1e8b564ea3cc22ff829
nekoze27 <teketeke.jimmy@gmail.com>
2023-03-27 08:44:18 +0000

    third commit

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\Admin\NewsController;
+Route::controller(NewsController::class)->prefix('admin')->group(function() {
+   Route::get('news/create','add'); 
+});
+
+// 3
+use App\Http\Controllers\AAAController;
+Route::controller(AAAController::class)->group(function() {
+    Route::get('AAAController', 'bbb');
+});
+
+// 4
+use App\Http\Controller\Admin\ProfileController;
+Route::Controller(ProfileController::class)->group(function() {
+    Route::get('admin/profile/create','add');
+    Route::get('admin/profaile/edit','edit');
+});
